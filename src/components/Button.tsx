cb128566a91068052b4585648872389354bd7af8
Simão Freire <simaofreire@live.com>
2024-01-15 15:26:39 -0300

    challenge done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,19 @@
 import { Icon } from './Icon';
 
-import '../styles/button.scss';
 import { ButtonHTMLAttributes } from 'react';
+import '../styles/button.scss';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string;
-  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  selected: boolean;
+	title: string;
+	iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	selected: boolean;
 }
 
 export function Button({ iconName, title, selected, ...rest }: ButtonProps) {
-  return (
-    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
-      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
-      {title}
-    </button>
-  );
+	return (
+		<button type="button" {...(selected && { className: 'selected' })} {...rest}>
+			<Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+			{title}
+		</button>
+	);
 }
